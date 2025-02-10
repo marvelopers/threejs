632e2d1faa69610ef28abb21a2e2ffb3ded4c1b6
chloe.dev(클로이) <chloe.dev@kakaostyle.com>
2025-02-10 22:21:09 +0900

    feat: geometry

--- project01/chapter02-threejs/src/main.js
+++ project01/chapter02-threejs/src/main.js
@@ -1,7 +1,14 @@
 import "./style.css";
 import * as THREE from "three";
 
-const Scene = new THREE.Scene();
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.shadowMap.enabled = true;
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   60,
   window.innerWidth / window.innerHeight,
@@ -12,22 +19,47 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 8;
 camera.position.y = 2;
 
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+directionalLight.castShadow = true;
+directionalLight.position.set(3, 4, 5);
+directionalLight.lookAt(0, 0, 0);
+
+scene.add(directionalLight);
+
+const floorGeometry = new THREE.PlaneGeometry(20, 20);
+const floorMatreial = new THREE.MeshStandardMaterial({ color: 0xbbbbbb });
+const floor = new THREE.Mesh(floorGeometry, floorMatreial);
+floor.rotation.x = -Math.PI / 2;
+floor.receiveShadow = true;
+floor.castShadow = true;
+scene.add(floor);
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const meterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const meterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 const mesh = new THREE.Mesh(geometry, meterial);
+scene.add(mesh);
 
-Scene.add(mesh);
+const capsuleGeometry = new THREE.CapsuleGeometry(1, 2, 20, 30);
+const capsuleMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+const capsuleMesh = new THREE.Mesh(capsuleGeometry, capsuleMaterial);
+capsuleMesh.position.set(3, 1.75, 0);
+capsuleMesh.castShadow = true;
+capsuleMesh.receiveShadow = true;
+scene.add(capsuleMesh);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement);
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.update();
 
 window.addEventListener("resize", () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.render(Scene, camera);
+  renderer.render(scene, camera);
 });
 
-renderer.render(Scene, camera);
+const render = () => {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+};
+
+render();
