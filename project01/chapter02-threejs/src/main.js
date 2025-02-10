806377a7922f97e09613c5b26c974a48d39a8ab1
chloe.dev(클로이) <chloe.dev@kakaostyle.com>
2025-02-10 21:51:35 +0900

    feat: renderer, scene, mesh, camera

--- project01/chapter02-threejs/src/main.js
+++ project01/chapter02-threejs/src/main.js
@@ -1,2 +1,33 @@
 import "./style.css";
 import * as THREE from "three";
+
+const Scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  60,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
+
+camera.position.z = 8;
+camera.position.y = 2;
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const meterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const mesh = new THREE.Mesh(geometry, meterial);
+
+Scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.render(Scene, camera);
+});
+
+renderer.render(Scene, camera);
